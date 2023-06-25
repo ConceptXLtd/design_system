0a3f177104e0af1f1164528203ed2035356f4778
ConceptXLtd <asad.soppiya@gmail.com>
2023-06-25 11:43:51 +0600

    all icons

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-export {Text} from "./text/text"
-export {Icon} from "./icon/icon"
-export {Button} from "./button/Button"
+import Text from "./text/text"
+import Icon from "./icon/icon"
+import Button from "./button/Button"
+
+module.exports = {
+    Text,
+    Icon,
+    Button
+}
