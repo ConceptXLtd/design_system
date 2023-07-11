5909df013f42eb97c400ed487b9f57863cf81dc8
ConceptXLtd <asad.soppiya@gmail.com>
2023-07-11 12:11:21 +0600

    froalajjnjn

---
+++ test/test2.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Widget() {
+    return (
+        <div style={{height: "100%", width: "100%", backgroundColor: "#454545"}}>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
