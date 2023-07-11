cafd7c762c8efa7b2439abf03d7ec6e7e5735a31
ConceptXLtd <asad.soppiya@gmail.com>
2023-07-11 10:58:42 +0600

    froala

---
+++ test/test.jsx
@@ -0,0 +1,7 @@
+export default function Widget(){
+    return (
+        <div style={{height: "100%", width: "100%", backgroundColor: "#454545"}}>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
