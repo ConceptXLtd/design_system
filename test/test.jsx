87f402698eae85e665621cf59715209c0c427a86
ConceptXLtd <asad.soppiya@gmail.com>
2023-07-11 11:51:45 +0600

    froala

--- test/test.jsx
+++ test/test.jsx
@@ -1,4 +1,4 @@
-export default function Widget(){
+function Widget(){
     return (
         <div style={{height: "100%", width: "100%", backgroundColor: "#454545"}}>
             <h1>Hello world</h1>
